2b048752416d481386ae6ec397f18c7aede37f9e
Blake Brandon <50201165+blakebrandon-hub@users.noreply.github.com>
2025-09-13 14:13:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -13,3 +13,4 @@ def paintings():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
