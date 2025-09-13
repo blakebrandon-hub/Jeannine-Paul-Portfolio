495afd3c782d7dd1335551456fe32bf07a0f4387
Blake Brandon <50201165+blakebrandon-hub@users.noreply.github.com>
2025-09-13 14:21:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,11 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.get("/")
+@app.get("/paintings")
 def paintings():
     return render_template('paintings.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
